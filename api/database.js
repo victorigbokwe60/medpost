4f21a3cc41ae5da186554db1502b036c15101e36
victorblvck <victorigbokwe60@gmail.com>
2025-01-17 17:47:13 +0700

    Correct runtime version in vercel.json

--- api/database.js
+++ api/database.js
@@ -2,7 +2,7 @@ const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router(path.join(__dirname, "..", "db.json")); // Correctly specify absolute path
+const router = jsonServer.router(path.join(__dirname, "..", "db.json"));
 
 server.use(middlewares);
 server.use(router);
