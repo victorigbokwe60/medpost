d15d4d50ee362769ebe4b2929d68a6731c977116
victorblvck <victorigbokwe60@gmail.com>
2025-01-17 17:28:11 +0700

    Specify absolute path for db.json and correct deployment configuration

--- api/database.js
+++ api/database.js
@@ -1,7 +1,8 @@
+const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const router = jsonServer.router(path.join(__dirname, "..", "db.json")); // Correctly specify absolute path
 
 server.use(middlewares);
 server.use(router);
